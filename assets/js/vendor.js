cc25d5fb1ee3c396284ceae7d12e5a3dffe5bd54
camilo <camilo@promusica.co>
2022-10-25 19:11:52 +0200

    commit JS Optimisation: vendor and assets dot js

---
+++ assets/js/vendor.js
@@ -0,0 +1,17 @@
+import { 
+    Alert, 
+    Button, 
+    Carousel, 
+    Collapse, 
+    Dropdown, 
+    Modal, 
+    Offcanvas, 
+    Popover, 
+    ScrollSpy, 
+    Tab, 
+    Toast, 
+    Tooltip 
+} from 'bootstrap';
+
+
+
